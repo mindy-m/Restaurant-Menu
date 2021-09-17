df2225315ebe1f38c42644c0160a131c4c173eda
Mindy Medeiros <mmedei6@wgu.edu>
2021-09-16 21:12:19 -0400

    Added fields

--- src/Restaurant/MenuItem.java
+++ src/Restaurant/MenuItem.java
@@ -2,5 +2,19 @@ package Restaurant;
 
 import java.util.*;
 
+/*
+
+    The menu consists of several menu items
+    Each menu item has a price, description, and category (appetizer, main course, or dessert)
+    It should be possible to display whether a menu item is new or not
+    The app should know when the menu was last updated, so visitors can see that the restaurant is constantly changing
+     and adding exciting new items
+
+ */
 public class MenuItem {
+    double price;
+    String description;
+    String category;
+    boolean isNew;
+    // need something for last updated - type?
 }
